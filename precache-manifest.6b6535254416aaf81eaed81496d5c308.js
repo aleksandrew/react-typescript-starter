31f6101cd711688be046fd4465d4b109a8ab851d
Alexandrew <andrew.alexandrow97@gmail.com>
2020-05-20 15:04:11 +0300

    Updates

---
+++ precache-manifest.6b6535254416aaf81eaed81496d5c308.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853d9d51ef9c9f30e87596fd38d09392",
+    "url": "/react-typescript-starter/index.html"
+  },
+  {
+    "revision": "fe2f73d7a91a79b44a05",
+    "url": "/react-typescript-starter/static/js/2.cb814143.chunk.js"
+  },
+  {
+    "revision": "6f10b10d869246cc4c29",
+    "url": "/react-typescript-starter/static/js/main.389dcf0d.chunk.js"
+  },
+  {
+    "revision": "6be4b754e9a085e7e10d",
+    "url": "/react-typescript-starter/static/js/runtime-main.b6d0234b.js"
+  }
+]);
